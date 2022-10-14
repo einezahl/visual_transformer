052168e028890272bf194741d1db2368f3b87785
reclik <reclik@imm.rwth-aachen.de>
2022-10-14 12:42:18 +0200

    Refactored tokenizer class

--- components/tokenizer.py
+++ components/tokenizer.py
@@ -11,13 +11,24 @@ class FilterTokenLayer(nn.Module):
 
     def __init__(self, n_token: int, n_channel: int) -> None:
         super().__init__()
-        self.w = nn.Linear(n_channel, n_token, False)
-        nn.init.xavier_uniform_(self.w.weight)
+        self.w_a = nn.Linear(n_channel, n_token, False)
+        nn.init.xavier_uniform_(self.w_a.weight)
 
-    def forward(self, x):
-        t = F.softmax(self.w(x.transpose(1, 2)), dim=1).transpose(1, 2)
-        t = torch.matmul(t, x.transpose(1, 2))
-        return t
+    def forward(self, feature_map: torch.Tensor) -> torch.Tensor:
+        """Forwards pass through the tokinzer layer
+
+        Args:
+            feature_map (torch.Tensor): Output of the feature extractor, has the shape (batch_size,
+            n_channel, feature_width, feature_height)
+
+        Returns:
+            torch.Tensor: Tokenized feature map, has the shape (batch_size, n_token, n_channel)
+        """
+        visual_token = F.softmax(
+            self.w_a(feature_map.transpose(1, 2)), dim=1
+        ).transpose(1, 2)
+        visual_token = torch.matmul(visual_token, feature_map.transpose(1, 2))
+        return visual_token
 
 
 class RecurrentTokenLayer(nn.Module):
@@ -27,11 +38,23 @@ class RecurrentTokenLayer(nn.Module):
 
     def __init__(self, n_channel: int) -> None:
         super().__init__()
-        self.w = nn.Linear(n_channel, n_channel, False)
-        nn.init.xavier_uniform_(self.w.weight)
+        self.w_t_r = nn.Linear(n_channel, n_channel, False)
+        nn.init.xavier_uniform_(self.w_t_r.weight)
 
-    def forward(self, x, t_in):
-        w = self.w(t_in)
-        t = F.softmax(torch.matmul(w, x), dim=1)
-        t = torch.matmul(t, x.transpose(1, 2))
-        return t
+    def forward(self, feature_map: torch.Tensor, visual_token_in: torch.Tensor):
+        """Forwards pass through the recurrent token layer, the weights for the tokenization are
+        dependent on the weights of the previous layer
+
+        Args:
+            feature_map (torch.Tensor): Output of the feature extractor, has the shape (batch_size,
+            n_channel, feature_width, feature_height)
+            visual_token_in (torch.Tensor): visual_token of the previous layer, has the shape
+            (batch_size, n_token, n_channel)
+
+        Returns:
+            _type_: _description_
+        """
+        w_r = self.w_t_r(visual_token_in)
+        t_out = F.softmax(torch.matmul(w_r, feature_map), dim=1)
+        t_out = torch.matmul(t_out, feature_map.transpose(1, 2))
+        return t_out
