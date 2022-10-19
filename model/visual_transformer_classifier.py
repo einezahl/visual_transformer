b77a3d02edefc0ee54764ecb5a73e29737251150
Tom Reclik <tom.reclik@gmail.com>
2022-10-19 17:33:27 +0200

    Added visual token classifier generator

--- model/visual_transformer_classifier.py
+++ model/visual_transformer_classifier.py
@@ -69,3 +69,45 @@ def create_model(
         classifier=classifier,
     )
     return visual_transformer_classifier
+
+
+def create_model_cuda(
+    n_token_layer: int,
+    n_token: int,
+    n_channel: int,
+    n_hidden: int,
+    n_classes: int,
+    device: torch.device,
+) -> VisualTransformerClassifier:
+    """Creates a visual transformer classifier
+    Args:
+        n_token_layer (int): Number of token layer
+        n_token (int): Number of visual token per layer
+        n_channel (int): Number of channel of the output of the feature extractor
+        n_hidden (int): Number of hidden modules in the transformer
+        n_classes (int): Number of classes
+    Returns:
+        VisualTransformerClassifier: Composite Classifier
+    """
+    resnet18_top = ResNet18Top()
+    resnet18_top.to(device)
+    tokenizer = Tokenizer(
+        n_token_layer=n_token_layer, n_token=n_token, n_channel=n_channel
+    )
+    tokenizer.to_device(device)
+    transformer = Transformer(n_channel=n_channel, n_hidden=n_hidden)
+    transformer.to(device)
+    projector = Projector(n_channel=n_channel)
+    projector.to(device)
+    visual_transformer = VisualTransformer(
+        tokenizer=tokenizer, transformer=transformer, projector=projector
+    )
+    visual_transformer.to(device)
+    classifier = ResNet18Classifier(in_features=n_channel, out_features=n_classes)
+    classifier.to(device)
+    visual_transformer_classifier = VisualTransformerClassifier(
+        feature_extractor=resnet18_top,
+        visual_transformer=visual_transformer,
+        classifier=classifier,
+    )
+    return visual_transformer_classifier
