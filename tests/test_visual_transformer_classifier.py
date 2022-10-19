37699e00fcc6b660d2b873472a0bd5be97106fcf
Tom Reclik <tom.reclik@gmail.com>
2022-10-19 18:08:33 +0200

    Added test of shape on GPU

--- tests/test_visual_transformer_classifier.py
+++ tests/test_visual_transformer_classifier.py
@@ -24,3 +24,26 @@ class TestVisualTransformerClassifier:
         )
         predictions = visual_transformer_classifier(image_batch)
         assert predictions.shape == (batch_size, 10)
+
+    def test_output_shape_cuda(self):
+        """Tests whether the model is correctly initilized when using a GPU"""
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        batch_size = 10
+        n_channel = 256
+        n_token = 16
+        n_token_layer = 6
+        n_hidden = 6
+        image_batch = torch.randn((batch_size, 3, 32, 32))
+        image_batch = image_batch.to(device)
+
+        visual_transformer_classifier = VisualTransformerClassifier(
+            n_token_layer=n_token_layer,
+            n_token=n_token,
+            n_channel=n_channel,
+            n_hidden=n_hidden,
+            n_classes=10,
+        )
+        visual_transformer_classifier.to(device)
+        predictions = visual_transformer_classifier(image_batch)
+        assert predictions.shape == (batch_size, 10)
