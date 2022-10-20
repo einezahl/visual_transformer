5de085c0a99695936ab321148bbe23e9ca64485c
Tom Reclik <tom.reclik@gmail.com>
2022-10-20 14:39:56 +0200

    Added trainer class

---
+++ utils/trainer.py
@@ -0,0 +1,35 @@
+import torch
+from torch import nn
+from torch import optim
+from torch.utils.data import DataLoader
+
+
+class Trainer:
+    def __init__(
+        self,
+        model: nn.Module,
+        loss: nn.Module,
+        optimizer: optim.Optimizer,
+        device: torch.device,
+    ) -> None:
+        self.model = model
+        self.loss = loss
+        self.optimizer = optimizer
+        self.device = device
+
+    def train(self, train_loader: DataLoader, epoch: int) -> None:
+        for i in range(epoch):
+            self.train_epoch(train_loader, i)
+
+    def train_epch(self, train_loader: DataLoader, epoch: int) -> None:
+        running_loss = 0.0
+        for i, (image_batch, label_batch) in enumerate(train_loader):
+            image_batch = image_batch.to(self.device)
+            label_batch = label_batch.to(self.device)
+            self.optimizer.zero_grad()
+            output = self.model(image_batch)
+            loss_value = self.loss(output, label_batch)
+            loss_value.backward()
+            self.optimizer.step()
+            running_loss += loss_value.item()
+        print(f"[{epoch + 1}] loss: {running_loss / 100:.3f}")
