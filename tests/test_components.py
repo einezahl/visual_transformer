abe568ab15a84ee01925c643ce599249b640b701
Tom Reclik <tom.reclik@gmail.com>
2022-10-20 18:51:18 +0200

    Added test to find whether all trainable parameters are also found by torch

--- tests/test_components.py
+++ tests/test_components.py
@@ -14,7 +14,7 @@ class TestComponents:
     next component
     """
 
-    def test_feature_extractor(self):
+    def test_feature_extractor_output_shape(self):
         """Test if the output of the ResNetReduced class has the correct shape
         for the CIFAR dataset"""
         batch_input = torch.randn((10, 3, 32, 32))
@@ -24,7 +24,63 @@ class TestComponents:
 
         assert preprocessed_data.shape == (10, 256, 2, 2)
 
-    def test_tokenizer_components(self):
+    def test_feature_extractor_torch_parameter(self):
+        """Test whether all parameters of ResNet18 network are found by torch"""
+        resnet18_top = ResNet18Top()
+
+        parameter = list(resnet18_top.parameters())
+
+        parameter_dimensions = []
+        parameter_dimensions.append([64, 3, 7, 7])
+        parameter_dimensions.append([64])
+        parameter_dimensions.append([64])
+        parameter_dimensions.append([64, 64, 3, 3])
+        parameter_dimensions.append([64])
+        parameter_dimensions.append([64])
+        parameter_dimensions.append([64, 64, 3, 3])
+        parameter_dimensions.append([64])
+        parameter_dimensions.append([64])
+        parameter_dimensions.append([64, 64, 3, 3])
+        parameter_dimensions.append([64])
+        parameter_dimensions.append([64])
+        parameter_dimensions.append([64, 64, 3, 3])
+        parameter_dimensions.append([64])
+        parameter_dimensions.append([64])
+        parameter_dimensions.append([128, 64, 3, 3])
+        parameter_dimensions.append([128])
+        parameter_dimensions.append([128])
+        parameter_dimensions.append([128, 128, 3, 3])
+        parameter_dimensions.append([128])
+        parameter_dimensions.append([128])
+        parameter_dimensions.append([128, 64, 1, 1])
+        parameter_dimensions.append([128])
+        parameter_dimensions.append([128])
+        parameter_dimensions.append([128, 128, 3, 3])
+        parameter_dimensions.append([128])
+        parameter_dimensions.append([128])
+        parameter_dimensions.append([128, 128, 3, 3])
+        parameter_dimensions.append([128])
+        parameter_dimensions.append([128])
+        parameter_dimensions.append([256, 128, 3, 3])
+        parameter_dimensions.append([256])
+        parameter_dimensions.append([256])
+        parameter_dimensions.append([256, 256, 3, 3])
+        parameter_dimensions.append([256])
+        parameter_dimensions.append([256])
+        parameter_dimensions.append([256, 128, 1, 1])
+        parameter_dimensions.append([256])
+        parameter_dimensions.append([256])
+        parameter_dimensions.append([256, 256, 3, 3])
+        parameter_dimensions.append([256])
+        parameter_dimensions.append([256])
+        parameter_dimensions.append([256, 256, 3, 3])
+        parameter_dimensions.append([256])
+        parameter_dimensions.append([256])
+
+        for i, param in enumerate(parameter):
+            assert param.shape == torch.Size(parameter_dimensions[i])
+
+    def test_tokenizer_components_output_shape(self):
         """Test if the output of the components of the tokenizer have the
         correct shape"""
         feature_map_input = torch.randn(10, 256, 4)
@@ -41,7 +97,30 @@ class TestComponents:
 
         assert second_layer_output.shape == (10, 16, 256)
 
-    def test_tokenizer(self):
+    def test_tokenizer_component_torch_parameter(self):
+        """Test whether all parameters of the components of the tokenizer are
+        found by torch"""
+        filter_token_layer = FilterTokenLayer(16, 256)
+        recurrent_token_layer = RecurrentTokenLayer(256)
+
+        filter_token_layer_parameter = list(filter_token_layer.parameters())
+        recurrent_token_layer_parameter = list(recurrent_token_layer.parameters())
+
+        filter_token_layer_parameter_dimensions = []
+        filter_token_layer_parameter_dimensions.append([16, 256])
+
+        recurrent_token_layer_parameter_dimensions = []
+        recurrent_token_layer_parameter_dimensions.append([256, 256])
+
+        for i, param in enumerate(filter_token_layer_parameter):
+            assert param.shape == torch.Size(filter_token_layer_parameter_dimensions[i])
+
+        for i, param in enumerate(recurrent_token_layer_parameter):
+            assert param.shape == torch.Size(
+                recurrent_token_layer_parameter_dimensions[i]
+            )
+
+    def test_tokenizer_output_shape(self):
         """Test if the output of the tokenizer has the correct shape"""
         feature_map_input = torch.randn(10, 256, 4)
 
@@ -50,7 +129,24 @@ class TestComponents:
 
         assert tokenizer_output.shape == (10, 16, 256)
 
-    def test_transformer(self):
+    def test_tokenizer_torch_parameter(self):
+        """Test whether all parameters of the tokenizer are found by torch"""
+        tokenizer = Tokenizer(n_token_layer=6, n_channel=256, n_token=16)
+
+        tokenizer_parameter = list(tokenizer.parameters())
+
+        tokenizer_parameter_dimensions = []
+        tokenizer_parameter_dimensions.append([16, 256])
+        tokenizer_parameter_dimensions.append([256, 256])
+        tokenizer_parameter_dimensions.append([256, 256])
+        tokenizer_parameter_dimensions.append([256, 256])
+        tokenizer_parameter_dimensions.append([256, 256])
+        tokenizer_parameter_dimensions.append([256, 256])
+
+        for i, param in enumerate(tokenizer_parameter):
+            assert param.shape == torch.Size(tokenizer_parameter_dimensions[i])
+
+    def test_transformer_output_shape(self):
         """Test if the output of the transformer has the correct shape"""
         visual_token_in = torch.randn(10, 16, 256)
         transformer = Transformer(n_channel=256, n_hidden=6)
@@ -59,7 +155,24 @@ class TestComponents:
 
         assert transformer_output.shape == (10, 16, 256)
 
-    def test_projector(self):
+    def test_transformer_torch_parameter(self):
+        """Test whether all parameters of the transformer are found by torch"""
+        transformer = Transformer(n_channel=256, n_hidden=32)
+
+        transformer_parameter = list(transformer.parameters())
+
+        transformer_parameter_dimensions = []
+        transformer_parameter_dimensions.append([256, 256])
+        transformer_parameter_dimensions.append([256, 256])
+        transformer_parameter_dimensions.append([32, 256])
+        transformer_parameter_dimensions.append([32])
+        transformer_parameter_dimensions.append([32, 256])
+        transformer_parameter_dimensions.append([32])
+
+        for i, param in enumerate(transformer_parameter):
+            assert param.shape == torch.Size(transformer_parameter_dimensions[i])
+
+    def test_projector_output_shape(self):
         """Test if the output of the projector has the correct shape"""
         feature_map_input = torch.randn(10, 256, 4)
         visual_token_in = torch.randn(10, 16, 256)
@@ -69,7 +182,22 @@ class TestComponents:
 
         assert projector_output.shape == (10, 256, 4)
 
-    def test_classifier(self):
+    def test_projector_torch_parameter(self):
+        """Test whether all parameters of the projector are found by torch"""
+        projector = Projector(n_channel=256)
+
+        projector_parameter = list(projector.parameters())
+
+        projector_parameter_dimensions = []
+        projector_parameter_dimensions.append([256, 256])
+        projector_parameter_dimensions.append([256])
+        projector_parameter_dimensions.append([256, 256])
+        projector_parameter_dimensions.append([256])
+
+        for i, param in enumerate(projector_parameter):
+            assert param.shape == torch.Size(projector_parameter_dimensions[i])
+
+    def test_classifier_output_shape(self):
         """Test if the output of the classifier has the correct shape"""
         feature_map_input = torch.randn(10, 256, 2, 2)
 
@@ -78,7 +206,20 @@ class TestComponents:
 
         assert classifier_output.shape == (10, 100)
 
-    def test_visual_transformer(self):
+    def test_classifier_torch_parameter(self):
+        """Test whether all parameters of the classifier are found by torch"""
+        classifier = ResNet18Classifier(256, 10)
+
+        classifier_parameter = list(classifier.parameters())
+
+        classifier_parameter_dimensions = []
+        classifier_parameter_dimensions.append([10, 256])
+        classifier_parameter_dimensions.append([10])
+
+        for i, param in enumerate(classifier_parameter):
+            assert param.shape == torch.Size(classifier_parameter_dimensions[i])
+
+    def test_visual_transformer_output_shape(self):
         """Test if the output of the classifier has the correct shape"""
         feature_map_input = torch.randn(10, 256, 2, 2)
 
@@ -91,3 +232,39 @@ class TestComponents:
         visual_transformer_output = visual_transformer(feature_map_input)
 
         assert visual_transformer_output.shape == (10, 256, 2, 2)
+
+    def test_visual_transformer_torch_parameter(self):
+        """Test whether all parameters of the classifier are found by torch"""
+
+        tokenizer = Tokenizer(n_token_layer=6, n_channel=256, n_token=16)
+        transformer = Transformer(n_channel=256, n_hidden=32)
+        projector = Projector(n_channel=256)
+        visual_transformer = VisualTransformer(
+            tokenizer=tokenizer, transformer=transformer, projector=projector
+        )
+
+        visual_transformer_parameter = list(visual_transformer.parameters())
+
+        visual_transformer_parameter_dimensions = []
+        visual_transformer_parameter_dimensions.append([16, 256])
+        visual_transformer_parameter_dimensions.append([256, 256])
+        visual_transformer_parameter_dimensions.append([256, 256])
+        visual_transformer_parameter_dimensions.append([256, 256])
+        visual_transformer_parameter_dimensions.append([256, 256])
+        visual_transformer_parameter_dimensions.append([256, 256])
+
+        visual_transformer_parameter_dimensions.append([256, 256])
+        visual_transformer_parameter_dimensions.append([256, 256])
+        visual_transformer_parameter_dimensions.append([32, 256])
+        visual_transformer_parameter_dimensions.append([32])
+        visual_transformer_parameter_dimensions.append([32, 256])
+        visual_transformer_parameter_dimensions.append([32])
+
+        visual_transformer_parameter_dimensions.append([256, 256])
+        visual_transformer_parameter_dimensions.append([256])
+        visual_transformer_parameter_dimensions.append([256, 256])
+        visual_transformer_parameter_dimensions.append([256])
+
+        for i, param in enumerate(visual_transformer_parameter):
+            print(i)
+            assert param.shape == torch.Size(visual_transformer_parameter_dimensions[i])
--- tests/test_visual_transformer_classifier.py
+++ tests/test_visual_transformer_classifier.py
@@ -47,3 +47,104 @@ class TestVisualTransformerClassifier:
         visual_transformer_classifier.to(device)
         predictions = visual_transformer_classifier(image_batch)
         assert predictions.shape == (batch_size, 10)
+
+    def test_torch_parameter(self):
+        """Test whether all parameters of the classifier are found by torch"""
+        n_channel = 256
+        n_token = 16
+        n_token_layer = 6
+        n_hidden = 32
+
+        visual_transformer_classifier = VisualTransformerClassifier(
+            n_token_layer=n_token_layer,
+            n_token=n_token,
+            n_channel=n_channel,
+            n_hidden=n_hidden,
+            n_classes=10,
+        )
+
+        visual_transformer_classifier_parameter = list(
+            visual_transformer_classifier.parameters()
+        )
+        # ResNet parameters
+        visual_transformer_classifier_parameter_dimensions = []
+        visual_transformer_classifier_parameter_dimensions.append([64, 3, 7, 7])
+        visual_transformer_classifier_parameter_dimensions.append([64])
+        visual_transformer_classifier_parameter_dimensions.append([64])
+        visual_transformer_classifier_parameter_dimensions.append([64, 64, 3, 3])
+        visual_transformer_classifier_parameter_dimensions.append([64])
+        visual_transformer_classifier_parameter_dimensions.append([64])
+        visual_transformer_classifier_parameter_dimensions.append([64, 64, 3, 3])
+        visual_transformer_classifier_parameter_dimensions.append([64])
+        visual_transformer_classifier_parameter_dimensions.append([64])
+        visual_transformer_classifier_parameter_dimensions.append([64, 64, 3, 3])
+        visual_transformer_classifier_parameter_dimensions.append([64])
+        visual_transformer_classifier_parameter_dimensions.append([64])
+        visual_transformer_classifier_parameter_dimensions.append([64, 64, 3, 3])
+        visual_transformer_classifier_parameter_dimensions.append([64])
+        visual_transformer_classifier_parameter_dimensions.append([64])
+        visual_transformer_classifier_parameter_dimensions.append([128, 64, 3, 3])
+        visual_transformer_classifier_parameter_dimensions.append([128])
+        visual_transformer_classifier_parameter_dimensions.append([128])
+        visual_transformer_classifier_parameter_dimensions.append([128, 128, 3, 3])
+        visual_transformer_classifier_parameter_dimensions.append([128])
+        visual_transformer_classifier_parameter_dimensions.append([128])
+        visual_transformer_classifier_parameter_dimensions.append([128, 64, 1, 1])
+        visual_transformer_classifier_parameter_dimensions.append([128])
+        visual_transformer_classifier_parameter_dimensions.append([128])
+        visual_transformer_classifier_parameter_dimensions.append([128, 128, 3, 3])
+        visual_transformer_classifier_parameter_dimensions.append([128])
+        visual_transformer_classifier_parameter_dimensions.append([128])
+        visual_transformer_classifier_parameter_dimensions.append([128, 128, 3, 3])
+        visual_transformer_classifier_parameter_dimensions.append([128])
+        visual_transformer_classifier_parameter_dimensions.append([128])
+        visual_transformer_classifier_parameter_dimensions.append([256, 128, 3, 3])
+        visual_transformer_classifier_parameter_dimensions.append([256])
+        visual_transformer_classifier_parameter_dimensions.append([256])
+        visual_transformer_classifier_parameter_dimensions.append([256, 256, 3, 3])
+        visual_transformer_classifier_parameter_dimensions.append([256])
+        visual_transformer_classifier_parameter_dimensions.append([256])
+        visual_transformer_classifier_parameter_dimensions.append([256, 128, 1, 1])
+        visual_transformer_classifier_parameter_dimensions.append([256])
+        visual_transformer_classifier_parameter_dimensions.append([256])
+        visual_transformer_classifier_parameter_dimensions.append([256, 256, 3, 3])
+        visual_transformer_classifier_parameter_dimensions.append([256])
+        visual_transformer_classifier_parameter_dimensions.append([256])
+        visual_transformer_classifier_parameter_dimensions.append([256, 256, 3, 3])
+        visual_transformer_classifier_parameter_dimensions.append([256])
+        visual_transformer_classifier_parameter_dimensions.append([256])
+
+        # Tokenizer parameters
+
+        visual_transformer_classifier_parameter_dimensions.append([16, 256])
+        visual_transformer_classifier_parameter_dimensions.append([256, 256])
+        visual_transformer_classifier_parameter_dimensions.append([256, 256])
+        visual_transformer_classifier_parameter_dimensions.append([256, 256])
+        visual_transformer_classifier_parameter_dimensions.append([256, 256])
+        visual_transformer_classifier_parameter_dimensions.append([256, 256])
+
+        # Transformer parameters
+
+        visual_transformer_classifier_parameter_dimensions.append([256, 256])
+        visual_transformer_classifier_parameter_dimensions.append([256, 256])
+        visual_transformer_classifier_parameter_dimensions.append([32, 256])
+        visual_transformer_classifier_parameter_dimensions.append([32])
+        visual_transformer_classifier_parameter_dimensions.append([32, 256])
+        visual_transformer_classifier_parameter_dimensions.append([32])
+
+        # Projector parameters
+
+        visual_transformer_classifier_parameter_dimensions.append([256, 256])
+        visual_transformer_classifier_parameter_dimensions.append([256])
+        visual_transformer_classifier_parameter_dimensions.append([256, 256])
+        visual_transformer_classifier_parameter_dimensions.append([256])
+
+        # Classifier parameters
+
+        visual_transformer_classifier_parameter_dimensions.append([10, 256])
+        visual_transformer_classifier_parameter_dimensions.append([10])
+
+        for i in range(len(visual_transformer_classifier_parameter)):
+            assert visual_transformer_classifier_parameter[i].shape == torch.Size(
+                visual_transformer_classifier_parameter_dimensions[i]
+            )
